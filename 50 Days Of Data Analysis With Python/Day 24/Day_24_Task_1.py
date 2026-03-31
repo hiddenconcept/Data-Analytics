ce07351bea49efff205215ff2fd6819ad41b2729
hiddenconcept <myles.patricio@gmail.com>
2026-03-30 22:14:54 -0400

    Delete 50 Days Of Data Analysis With Python/Day 24 directory

--- 50 Days Of Data Analysis With Python/Day 24/Day_24_Task_1.py
+++
@@ -1,9 +0,0 @@
-#Using pandas import the Excel file above.
-# Write a code to view the first 5 rows of your DataFrame.
-
-import pandas as pd
-
-df = pd.read_excel('Asset_sales_data.xlsx')
-
-# View the first 5 rows
-print("\nFirst 5 rows:\n",df.head())
