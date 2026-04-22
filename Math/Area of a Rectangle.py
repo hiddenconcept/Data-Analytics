86ea49bcfa390ba9fa93ea3ce74508fe8e734dab
hiddenconcept <myles.patricio@gmail.com>
2026-04-21 22:29:12 -0400

    Add files via upload

---
+++ Math/Area of a Rectangle.py
@@ -0,0 +1,6 @@
+width = int(input("Enter the width of the rectangle: "))
+height = int(input("Enter the height of the rectangle: "))
+
+Area = width * height
+
+print("\nThe area of the rectangle is:\n", Area)
