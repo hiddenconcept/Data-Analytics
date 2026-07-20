63d805815a812c89908089a13c29554bfc4435a0
Myles Patricio <myles.patricio@gmail.com>
2026-07-19 21:09:30 -0400

    Add files via upload

---
+++ 50 Days Of Data Analysis With Python/Day 49/Day_49_Task_1.py
@@ -0,0 +1,4 @@
+#1 What is meant by "regression" in supervised machine learning?
+
+#Regression is a supervised learning technique used to predict a continuous numerical output by,
+# learning the relationship between input features and known target values.
